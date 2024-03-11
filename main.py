ae83be3561b9ff5da6e929e37daa1499b4f88945
Your Name <you@example.com>
2024-03-11 22:08:12 +0530

    training of the updated base model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 # from cnnClassifier.pipeline.stage_03_prepare_callbacks import PrepareCallBacksPipeline
 
 
@@ -26,3 +27,18 @@ try:
 except  Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Training"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
